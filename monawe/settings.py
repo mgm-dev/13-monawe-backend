ea4121d5d03c35a3a7a17d9b76c0802b8ccfc7b0
Min Gyeong-Min <tommin231@gmail.com>
2020-10-20 19:25:22 +0900

    Edit : settings.py에 user, order 앱 추가

--- monawe/settings.py
+++ monawe/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'product'
+    'user',
+    'product',
+    'order'
 ]
 
 MIDDLEWARE = [
