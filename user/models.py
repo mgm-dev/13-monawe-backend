4a0a6d5888f81d8a81bc327f8cb64e151f93d3e0
Min Gyeong-Min <tommin231@gmail.com>
2020-10-20 19:46:02 +0900

    Add : 주소지를 저장하는  addresses 테이블 추가

--- user/models.py
+++ user/models.py
@@ -1,16 +1,27 @@
+# autopep8: off
 from django.db import models
 
 
 class Users(models.Model):
-    account = models.CharField(max_length=45)
-    password = models.CharField(max_length=600)
-    name = models.CharField(max_length=45)
-    email = models.EmailField(max_length=254)
-    phone_number = models.CharField(max_length=45)
+    account       = models.CharField(max_length=45)
+    password      = models.CharField(max_length=600)
+    name          = models.CharField(max_length=45)
+    email         = models.EmailField(max_length=254)
+    phone_number  = models.CharField(max_length=45)
     date_of_birth = models.DateField(
         auto_now=False, auto_now_add=False, null=True)
-    sms_agreement = models.BooleanField()
+    sms_agreement   = models.BooleanField()
     email_agreement = models.BooleanField()
 
     class Meta:
         db_table = "users"
+
+
+class Addresses(models.Model):
+    user_id          = models.ForeignKey(Users, on_delete=models.CASCADE)
+    address          = models.CharField(max_length=200)
+    detailed_address = models.CharField(max_length=200)
+    zip_code         = models.IntegerField()
+
+    class Meta:
+        db_table = "addresses"
