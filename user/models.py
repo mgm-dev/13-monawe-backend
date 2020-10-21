db2275fed73acc3666d65829be37f1f348e58b6d
Min Gyeong-Min <tommin231@gmail.com>
2020-10-21 11:25:00 +0900

    Refactor : models.py & views.py 리팩토링 - 클래스명 단수형으로 수정 - vertical align

--- user/models.py
+++ user/models.py
@@ -2,13 +2,13 @@
 from django.db import models
 
 
-class Users(models.Model):
-    account       = models.CharField(max_length=45)
-    password      = models.CharField(max_length=600)
-    name          = models.CharField(max_length=45)
-    email         = models.EmailField(max_length=254)
-    phone_number  = models.CharField(max_length=45)
-    date_of_birth = models.DateField(
+class User(models.Model):
+    account         = models.CharField(max_length=45)
+    password        = models.CharField(max_length=600)
+    name            = models.CharField(max_length=45)
+    email           = models.EmailField(max_length=254)
+    phone_number    = models.CharField(max_length=45)
+    date_of_birth   = models.DateField(
         auto_now=False, auto_now_add=False, null=True)
     sms_agreement   = models.BooleanField()
     email_agreement = models.BooleanField()
@@ -17,7 +17,7 @@ class Users(models.Model):
         db_table = "users"
 
 
-class Addresses(models.Model):
+class Address(models.Model):
     user_id          = models.ForeignKey(Users, on_delete=models.CASCADE)
     address          = models.CharField(max_length=200)
     detailed_address = models.CharField(max_length=200)
