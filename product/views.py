9599f88ac2f41eed50d8b203d07e01267982efac
Min Gyeong-Min <tommin231@gmail.com>
2020-10-22 10:00:48 +0900

    Add : 상품 목록 엔드포인트 작성 - all : 모든 상품 정보 리턴 - category : 상품 중분류로 필터링 - subcategory : 상품 소분류로 필터링

--- product/views.py
+++ product/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from product.models import Product, Category
+from product.models import Subcategory as SC
+from django.views import View
+from django.http import JsonResponse
+
+
+class All(View):
+    def get(self, request):
+        data = list(Product.objects.all().values())
+        return JsonResponse(data, safe=False)
+
+
+class Category(View):
+    def get(self, request, category_id):
+        subcategories = SC.objects.filter(
+            category_id=category_id)
+        data = list(Product.objects.filter(
+            subcategory__in=subcategories).values())
+        return JsonResponse(data, safe=False)
+
+
+class Subcategory(View):
+    def get(self, request, subcategory_id):
+        data = list(Product.objects.filter(
+            subcategory_id=subcategory_id).values())
+        return JsonResponse(data, safe=False)
