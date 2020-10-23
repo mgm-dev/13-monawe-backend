e65b1ec94140a0c3345f8ee7f51e0786734c3761
Min Gyeong-Min <tommin231@gmail.com>
2020-10-23 13:57:58 +0900

    Add : 상품 상세 정보를 리턴 하는 Detail 뷰 작성

--- product/views.py
+++ product/views.py
@@ -84,3 +84,41 @@ class Subcategory(View):
 
         return JsonResponse(products, safe=False)
 
+class Detail(View):
+    def get(self, request, product_id):
+        product = Product.objects.get(id=product_id)
+
+        bodyColor = []
+        inkColor = []
+
+        for product_option in ProductOption.objects.filter(product=product):
+            body = {}
+            body['name'] = product_option.body_color.name
+            body['hex'] = product_option.body_color.hex_code
+
+            ink = {}
+            ink['name'] = product_option.ink_color.name
+            ink['hex'] = product_option.ink_color.hex_code
+
+            bodyColor.append(body)
+            inkColor.append(ink)
+
+        data = {
+            'productInfo': {
+                'name': product.name,
+                'imageUrl': [product_image.image_url for product_image in ProductImage.objects.filter(product=product)],
+                'bodyColor': bodyColor,
+                'inkColor': inkColor,
+                'thickness': [product_option.thickness.value for product_option in ProductOption.objects.filter(product=product)],
+                'description': product.description,
+                'tag': [productTag.tag.name for productTag in ProductTag.objects.filter(product=product)],
+                'price': product.price,
+                'options': [product_option.body_color.name + "/" + product_option.ink_color.name
+                            + "(" + product_option.thickness.value + "mm)"
+                            + "(재고:" + str(product_option.stock) + "개)"
+                            for product_option in ProductOption.objects.filter(product=product)],
+                'stock': [product_option.stock for product_option in ProductOption.objects.filter(product=product)]
+            }
+        }
+
+        return JsonResponse(data, safe=False)
