6f06e3cf9e65f7c2804c00cb5d001a76749e02aa
Min Gyeong-Min <tommin231@gmail.com>
2020-10-23 15:44:23 +0900

    Add : product.urls.py에 상품상세 정보 엔드포인트 추가

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from product.views import All, Category, Subcategory
+from product.views import All, Category, Subcategory, Detail
 
 urlpatterns = [
     path('all', All.as_view()),
     path('category/<int:category_id>', Category.as_view()),
     path('subcategory/<int:subcategory_id>', Subcategory.as_view()),
+    path('detail/<int:product_id>', Detail.as_view()),
 ]
