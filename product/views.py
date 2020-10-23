64e3e89f633ff09113c32b194d45d5e6c4f22e0d
Min Gyeong-Min <tommin231@gmail.com>
2020-10-23 16:50:58 +0900

    Add : 퀴리 스트링으로 정렬 기준 받고, 상품 리스트 정렬 해서 리턴 하는 기능 추가

--- product/views.py
+++ product/views.py
@@ -13,28 +13,46 @@ from django.http    import JsonResponse
 
 class All(View):
     def get(self, request):
+        order_by = request.GET.get('order_by', None)
         products = []
-        for product in Product.objects.all():
-            products.append(product.get_info())
 
-        return JsonResponse({ "productList" : products}, status=200)
+        if order_by:
+            for product in Product.objects.all().order_by(order_by):
+                products.append(product.get_info())
+        else:
+            for product in Product.objects.all():
+                products.append(product.get_info())
+
+        return JsonResponse({ "data" : products}, status=200)
 
 class Category(View):
     def get(self, request, category_id):
+        order_by = request.GET.get('order_by', None)
         subcategories = SC.objects.filter(category_id=category_id)
         products = []
-        for product in Product.objects.filter(subcategory__in=subcategories):
-            products.append(product.get_info())
 
-        return JsonResponse({ "productList" : products}, status=200)
+        if order_by:
+            for product in Product.objects.filter(subcategory__in=subcategories).order_by(order_by):
+                products.append(product.get_info())
+        else:
+            for product in Product.objects.filter(subcategory__in=subcategories):
+                products.append(product.get_info())
+
+        return JsonResponse({ "data" : products}, status=200)
 
 class Subcategory(View):
     def get(self, request, subcategory_id):
+        order_by = request.GET.get('order_by', None)
         products = []
-        for product in Product.objects.filter(subcategory_id=subcategory_id):
-            products.append(product.get_info())
 
-        return JsonResponse({ "productList" : products}, status=200)
+        if order_by:
+            for product in Product.objects.filter(subcategory_id=subcategory_id).order_by(order_by):
+                products.append(product.get_info())
+        else:
+            for product in Product.objects.filter(subcategory_id=subcategory_id):
+                products.append(product.get_info())
+
+        return JsonResponse({ "data" : products}, status=200)
 
 class Detail(View):
     def get(self, request, product_id):
@@ -73,4 +91,4 @@ class Detail(View):
             }
         }
 
-        return JsonResponse(data, safe=False)
+        return JsonResponse({ "data" : data}, status=200)
