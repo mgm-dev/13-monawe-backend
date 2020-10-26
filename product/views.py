d0003363f1223ecd0f29f9afa7a1ff0e20fd591e
Min Gyeong-Min <tommin231@gmail.com>
2020-10-26 22:47:13 +0900

    Refactor : pagination 모듈 사용하지 않고 페이지네이션 구현

--- product/views.py
+++ product/views.py
@@ -6,7 +6,6 @@ from pathlib                import Path
 from django.http            import JsonResponse
 from django.db              import IntegrityError
 from django.core            import serializers
-from django.core.paginator  import Paginator
 from django.core.exceptions import FieldError
 from django.http            import JsonResponse
 from django.utils           import timezone
@@ -17,27 +16,33 @@ from user.models    import User
 class ProductList(View):
     def get(self, request):
         try:
-            order_by         = request.GET.get('order_by', None)
-            page_number      = request.GET.get('page_number', None)
-            item_per_page    = request.GET.get('item_per_page', None)
-            category_id      = request.GET.get('category', None)
-            subcategory_id   = request.GET.get('subcategory', None)
+            order_by       = request.GET.get('order_by', None)
+            page_number    = int(request.GET.get('page_number', None))
+            item_per_page  = int(request.GET.get('item_per_page', None))
+            category_id    = request.GET.get('category', None)
+            subcategory_id = request.GET.get('subcategory', None)
+            offset         = (page_number - 1) * item_per_page
 
             if subcategory_id:
-                product_list  = Product.objects.filter(subcategory_id=subcategory_id).order_by(order_by)
+                product_count = Product.objects.filter(subcategory_id=subcategory_id).count()
+                num_pages     = product_count // item_per_page if product_count % item_per_page == 0 else product_count // item_per_page + 1;
+                product_list  = Product.objects.filter(subcategory_id=subcategory_id).order_by(order_by)[offset:offset+item_per_page]
             elif category_id:
                 subcategories = Subcategory.objects.filter(category_id=category_id)
-                product_list  = Product.objects.filter(subcategory__in=subcategories).order_by(order_by)
+                product_count = Product.objects.filter(subcategory__in=subcategories).count()
+                num_pages     = product_count // item_per_page if product_count % item_per_page == 0 else product_count // item_per_page + 1;
+                product_list  = Product.objects.filter(subcategory__in=subcategories).order_by(order_by)[offset:offset+item_per_page]
             else:
-                product_list  = Product.objects.all().order_by(order_by)
+                product_count = Product.objects.all().count()
+                num_pages     = product_count // item_per_page if product_count % item_per_page == 0 else product_count // item_per_page + 1;
+                product_list  = Product.objects.all().order_by(order_by)[offset:offset+item_per_page]
 
-            paginator = Paginator(product_list, item_per_page)
-            data      = []
+            data = []
 
-            for product in paginator.get_page(page_number):
+            for product in product_list:
                 data.append(product.get_info())
 
-            return JsonResponse({ "numPages" : paginator.num_pages, "data" : data}, status=200)
+            return JsonResponse({ "numPages" : num_pages, "data" : data}, status=200)
 
         except ValueError:
             return JsonResponse({ "message" : "VALUE_ERROR"}, status=400)
@@ -45,6 +50,7 @@ class ProductList(View):
             return JsonResponse({ "message" : "TYPE_ERROR"}, status=400)
         except FieldError:
             return JsonResponse({ "message" : "FIELD_ERROR"}, status=400)
+
 
 class Search(View):
     def get(self,request):
