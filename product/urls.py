9d748aa0ff6c45f6186dc9235aaf849e5ac64140
Min Gyeong-Min <tommin231@gmail.com>
2020-10-26 13:30:30 +0900

    Add : review url 추가

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from product.views import Detail, Search, ProductList
+from product.views import Detail, Search, ProductList, Review
 
 urlpatterns = [
     path('list', ProductList.as_view()),
     path('detail/<int:product_id>', Detail.as_view()),
-    path('search', Search.as_view())
+    path('search', Search.as_view()),
+    path('review', Review.as_view())
 ]
