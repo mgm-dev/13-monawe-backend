ff6e53421aee657d984277c82b53cb4687e5d791
Min Gyeong-Min <tommin231@gmail.com>
2020-10-26 14:30:16 +0900

    Debug : review 뷰 디버깅

--- product/views.py
+++ product/views.py
@@ -127,6 +127,8 @@ class Review(View):
         elif review_id:
             review_data = ProductReview.objects.filter(id = review_id).values()
             review_list = [review for review in review_data]
+            if len(review_list) == 0:
+                return JsonResponse({'MESSAGE': 'REVIEW_DOES_NOT_EXIST'}, status = 404) 
 
         return JsonResponse({'REVIEWS': review_list}, status = 200)
 
@@ -138,8 +140,8 @@ class Review(View):
             return JsonResponse({'MESSAGE': 'ALREADY_WROTE_REVIEW'}, status = 400)
         else:
             ProductReview(
-                user_id     = User.objects.get(id = data['user_id']),
-                product_id  = target_product,
+                user    = User.objects.get(id = data['user_id']),
+                product  = target_product,
                 rating      = data['rating'],
                 title       = data['title'],
                 content     = data['content'],
@@ -150,26 +152,25 @@ class Review(View):
 
     def patch(self, request):
         data           = json.loads(request.body)
-        target_review  = Product.objects.get(id = data['review_id'])
+        target_review  = ProductReview.objects.get(id = data['review_id'])
 
-        if not ProductReview.objects.filter(id = target_review).exists():
+        if not ProductReview.objects.filter(id = target_review.id).exists():
             return JsonResponse({'MESSAGE': 'REVIEW_DOES_NOT_EXIST'}, status = 404)
         else:
-            ProductReview(
+            ProductReview.objects.filter(id = target_review.id).update(
                 rating      = data['rating'],
                 title       = data['title'],
                 content     = data['content'],
                 image_url   = data['image_url'],
-            ).save()
-
+            )
             return JsonResponse({'MESSAGE':'REVIEW_UPDATED'}, status = 201)
 
     def delete(self, request):
         data           = json.loads(request.body)
-        target_review  = Product.objects.get(id = data['review_id'])
+        target_review  = ProductReview.objects.get(id = data['review_id'])
 
-        if not ProductReview.objects.filter(id = target_review).exists():
+        if not ProductReview.objects.filter(id = target_review.id).exists():
             return JsonResponse({'MESSAGE': 'REVIEW_DOES_NOT_EXIST'}, status = 404)
         else:
-            ProductReview.objects.filter(id = target_review).delete()
+            ProductReview.objects.filter(id = target_review.id).delete()
             return JsonResponse({'MESSAGE': 'REVIEW_DELETED'}, status = 200)
