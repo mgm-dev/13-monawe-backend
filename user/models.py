c4d217bed076ea8c3f70e4445d19e19b9d2316d8
HwangNina <hwangninaa@gmail.com>
2020-10-27 10:51:17 +0900

    [ADD] user모델 addresses 테이블에 컬럼 추가

--- user/models.py
+++ user/models.py
@@ -21,6 +21,7 @@ class Address(models.Model):
     address          = models.CharField(max_length=200)
     detailed_address = models.CharField(max_length=200)
     zip_code         = models.IntegerField()
+    is_default       = models.CharField(max_length=10, null=True)
 
     class Meta:
         db_table = "addresses"
