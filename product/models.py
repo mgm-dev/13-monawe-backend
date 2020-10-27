07022058b1e74853f4634d64fb3c1a39968b63bb
Min Gyeong-Min <tommin231@gmail.com>
2020-10-27 19:48:30 +0900

    Delete : review 앱으로 ProductReview 모델 이전

--- product/models.py
+++ product/models.py
@@ -128,18 +128,3 @@ class ProductTag(models.Model):
 
     class Meta:
         db_table = 'product_tags'
-
-# Review
-
-class ProductReview(models.Model):
-    user       = models.ForeignKey(User, on_delete=models.CASCADE)
-    product    = models.ForeignKey(Product, on_delete=models.CASCADE)
-    rating     = models.DecimalField(max_digits=2, decimal_places=1)
-    title      = models.CharField(max_length=45)
-    content    = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    image_url  = models.URLField(max_length=200, null=True)
-
-    class Meta():
-        db_table  = 'product_reviews'
