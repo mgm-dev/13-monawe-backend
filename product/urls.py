ffd8916b84ac26d99c388f158ddfd4cb93dcdb6f
Min Gyeong-Min <tommin231@gmail.com>
2020-10-27 19:55:05 +0900

    Refactor : url을 RESTful 하게 수정 - product/detail/1 => product/1 - product/list? => products?

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from product.views import Detail, ProductList
 
 urlpatterns = [
-    path('list', ProductList.as_view()),
-    path('detail/<int:product_id>', Detail.as_view())
+    path('s', ProductList.as_view()),
+    path('/<int:product_id>', Detail.as_view())
 ]
