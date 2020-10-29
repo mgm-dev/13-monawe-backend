c5e7b17c43e623f9f3b6f7452ea4525d7878ce46
Min Gyeong-Min <tommin231@gmail.com>
2020-10-29 10:38:31 +0900

    Edit : typo 수정 Authentication -> Authorization

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ from user.models            import User
 def signin_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            token     = request.headers.get('Authentication', None)
+            token     = request.headers.get('Authorization', None)
             key       = my_settings.SECRET.get('JWT_KEY')
             algorithm = my_settings.SECRET.get('JWT_ALGORITHM')
 
