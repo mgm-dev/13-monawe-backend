4b73909a06e6fbb016a73cf0b3492600b49c95db
Min Gyeong-Min <tommin231@gmail.com>
2020-10-29 10:05:58 +0900

    Edit : conflict 해결

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
-
-urlpatterns = []
-=======
 from product.views import Detail, ProductList
 
 urlpatterns = [
     path('s', ProductList.as_view()),
     path('/<int:product_id>', Detail.as_view())
 ]
->>>>>>> bde81ab1bd76595fae84451c3123e83ebe5e5adc
--- user/urls.py
+++ user/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
-<<<<<<< HEAD
 from user.views import SignUp, SignIn, CheckEmail, CheckAccount, UserInfo, AddressView
 
 urlpatterns = [
-    path('signup', SignUp.as_view()),
-    path('signin', SignIn.as_view()),
-    path('checkemail', CheckEmail.as_view()),
-    path('checkaccount', CheckAccount.as_view()),
-    path('userinfo', UserInfo.as_view()),
-    path('address', AddressView.as_view()),
+    path('/signup', SignUp.as_view()),
+    path('/signin', SignIn.as_view()),
+    path('/checkemail', CheckEmail.as_view()),
+    path('/checkaccount', CheckAccount.as_view()),
+    path('/userinfo', UserInfo.as_view()),
+    path('/address', AddressView.as_view()),
 ]
-=======
-
-urlpatterns = []
->>>>>>> bde81ab1bd76595fae84451c3123e83ebe5e5adc
