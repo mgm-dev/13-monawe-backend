f65618bb3eb08af2056baea812b9c2417455b11b
HwangNina <hwangninaa@gmail.com>
2020-10-29 16:36:14 +0900

    [EDIT] order view 수정

--- order/views.py
+++ order/views.py
@@ -16,103 +16,114 @@ class CartView(View):
 
     @utils.signin_decorator
     def post(self, request):
-        data            = json.loads(request.body)
-        user_id         = request.user.id
-        chosen_products = data['chosen_product']
+        try:
+            data            = json.loads(request.body)
+            user_id         = request.user.id
+            chosen_products = data['chosen_product']
 
-        target_cart, flag   = Order.objects.get_or_create(user_id = user_id, order_status_id = 1)
-        target_options      = [chosen_products[i]["product_option_id"] for i in range(0, len(chosen_products))]
+            target_cart, flag   = Order.objects.get_or_create(user_id = user_id, order_status_id = 1)
+            target_options      = [chosen_products[i]["product_option_id"] for i in range(0, len(chosen_products))]
 
-        for option in target_options:
-            if OrderProduct.objects.filter(order = target_cart.id, product_option = option).exists():
+            for option in target_options:
+                if OrderProduct.objects.filter(order = target_cart.id, product_option = option).exists():
 
-                return JsonResponse(
-                    {'message':'ALREADY_EXISTS'},
-                    status=409)
+                    return JsonResponse(
+                        {'message':'ALREADY_EXISTS'},
+                        status=409)
 
-        target_products = [ProductOption.objects.get(id = target_options[i]).product_id 
-                          for i in range(0, len(target_options))]
+            target_products = [ProductOption.objects.get(id = target_options[i]).product_id 
+                              for i in range(0, len(target_options))]
 
-        target_amount   = [chosen_products[i]['amount'] 
-                          for i in range(0, len(chosen_products))]
+            target_amount   = [chosen_products[i]['amount'] 
+                              for i in range(0, len(chosen_products))]
 
-        for j in range(0, len(target_options)):
-            OrderProduct(
-                product_amount = target_amount[j],
-                order_id = target_cart.id,
-                product_id = target_products[j],
-                product_option_id = target_options[j]
-            ).save()
+            for j in range(0, len(target_options)):
+                OrderProduct(
+                    product_amount = target_amount[j],
+                    order_id = target_cart.id,
+                    product_id = target_products[j],
+                    product_option_id = target_options[j]
+                ).save()
 
-        return JsonResponse(
-            {'message':'PRODUCT_ADDED'},
-            status=201
-        )
+            return JsonResponse(
+                {"message":"PRODUCT_ADDED"},
+                status=201
+            )
+
+        except KeyError:
+            return JsonResponse(
+                {"message":"KEY_ERROR"},
+                status=400
+            )
 
     @utils.signin_decorator
     def get(self, request):
-        user_id = request.user.id
-        target_order     = Order.objects.get(user = user_id, order_status = 1)
-        products_in_cart = [products for products in Order.objects.get(
-                           user = user_id, order_status = 1
-                           ).orderproduct_set.all().values()]
-        
-        product_in_cart        = [products['product_id'] for products in products_in_cart]
-        product_option_in_cart = [products["product_option_id"] for products in products_in_cart]
+            user_id = request.user.id
+            target_order     = Order.objects.get(user = user_id, order_status = 1)
+            products_in_cart = [products for products in Order.objects.get(
+                               user = user_id, order_status = 1
+                               ).orderproduct_set.all().values()]
 
-        product_name_list       = [Product.objects.get(id = p_id).name for p_id in product_in_cart]
-        product_thumbnail_list  = [Product.objects.get(id = p_id).thumb_nail for p_id in product_in_cart]
-        product_bodycolor_list  = [ProductOption.objects.get(id = p_id).body_color.name for p_id in product_option_in_cart]
-        product_price_list      = [Product.objects.get(id = p_id).price for p_id in product_in_cart]
-        product_plusprice_list  = [ProductOption.objects.get(id = p_id).plus_price for p_id in product_option_in_cart]
-        product_company_list    = [Product.objects.get(id = p_id).company for p_id in product_in_cart]
+            product_in_cart        = [products['product_id'] for products in products_in_cart]
+            product_option_in_cart = [products["product_option_id"] for products in products_in_cart]
 
-        list_per_product = []
-        for i in range(0, len(products_in_cart)):
-            product_price  = product_price_list[i] + product_plusprice_list[i]
-            product_amount = OrderProduct.objects.get(
-                             product_option = product_option_in_cart[i], 
-                             order          = target_order
-                             ).product_amount
-            total_price    = product_amount * product_price
+            product_name_list       = [Product.objects.get(id = p_id).name for p_id in product_in_cart]
+            product_thumbnail_list  = [Product.objects.get(id = p_id).thumb_nail for p_id in product_in_cart]
+            product_bodycolor_list  = [ProductOption.objects.get(id = p_id).body_color.name for p_id in product_option_in_cart]
+            product_price_list      = [Product.objects.get(id = p_id).price for p_id in product_in_cart]
+            product_plusprice_list  = [ProductOption.objects.get(id = p_id).plus_price for p_id in product_option_in_cart]
+            product_company_list    = [Product.objects.get(id = p_id).company for p_id in product_in_cart]
 
-            product_detail = {
-                "product_option_id"    : product_option_in_cart[i],
-                "product_name"         : product_name_list[i],
-                "product_thumbnail"    : product_thumbnail_list[i],
-                "product_bodycolor"    : product_bodycolor_list[i], 
-                "product_price"        : product_price,
-                "product_company"      : product_company_list[i],
-                "product_amount"       : product_amount,
-                "total_price"          : total_price
-            }
+            list_per_product = []
+            for i in range(0, len(products_in_cart)):
+                product_price  = product_price_list[i] + product_plusprice_list[i]
+                product_amount = OrderProduct.objects.get(
+                                 product_option = product_option_in_cart[i], 
+                                 order          = target_order
+                                 ).product_amount
+                total_price    = product_amount * product_price
 
-            list_per_product.append(product_detail)
+                product_detail = {
+                    "product_option_id"    : product_option_in_cart[i],
+                    "product_name"         : product_name_list[i],
+                    "product_thumbnail"    : product_thumbnail_list[i],
+                    "product_bodycolor"    : product_bodycolor_list[i], 
+                    "product_price"        : product_price,
+                    "product_company"      : product_company_list[i],
+                    "product_amount"       : product_amount,
+                    "total_price"          : total_price
+                }
+                list_per_product.append(product_detail)
 
-        return JsonResponse(
-            {'product_detail' : list_per_product},
-            status = 200
-        )
+            return JsonResponse(
+                {"product_detail" : list_per_product},
+                status=200
+            )
     # change the total amount
     @utils.signin_decorator
     def patch(self, request, product_option_id):
-        data            = json.loads(request.body)
-        user_id         = request.user.id
-        target_cart     = Order.objects.get(user = user_id, order_status = 1)
-        target_product  = ProductOption.objects.get(id = product_option_id)
-        product_in_cart = OrderProduct.objects.get(product_option = product_option_id, order = target_cart)
+        try:
+            data            = json.loads(request.body)
+            user_id         = request.user.id
+            target_cart     = Order.objects.get(user = user_id, order_status = 1)
 
-        product_price   = target_product.plus_price + Product.objects.get(id = target_product.product_id).price
+            target_product                  = ProductOption.objects.get(id = product_option_id)
+            product_in_cart                 = OrderProduct.objects.get(product_option = product_option_id, order = target_cart)
+            product_price                   = target_product.plus_price + Product.objects.get(id = target_product.product_id).price
+            product_in_cart.product_amount  = data['amount']
+            new_price                       = product_in_cart.product_amount * product_price
 
-        product_in_cart.product_amount = data['amount']
+            return JsonResponse(
+                {"message":"AMOUNT_CHANGED",
+                "total_price": new_price},
+                status = 200
+            )
 
-        new_price = product_in_cart.product_amount * product_price
-
-        return JsonResponse(
-            {'message':'AMOUNT_CHANGED',
-            'total_price': new_price},
-            status = 200
-        )
+        except KeyError:
+            return JsonResponse(
+                {"message":"KEY_ERROR"},
+                status=400
+            )
 
     @utils.signin_decorator
     def delete(self, request, product_option_id):
@@ -120,7 +131,7 @@ class CartView(View):
         target_cart     = Order.objects.get(user = user_id, order_status = 1)
 
         OrderProduct.objects.filter(product_option = product_option_id, order = target_cart).delete()
-        # to show the rest items
+        # to show the remaining items
         target_order     = Order.objects.get(user = user_id, order_status = 1)
         products_in_cart = [products for products in Order.objects.get(
                            user = user_id, order_status = 1
@@ -153,12 +164,11 @@ class CartView(View):
             list_per_product.append(product_detail)
 
         return JsonResponse(
-            {'message': 'DELETED',
-            'product_detail': list_per_product},
+            {"message": "DELETED",
+            "product_detail": list_per_product},
             status=200
         )
 
-# 주문기능 구현x T_T
 
 # class CheckoutView(View):
 
@@ -176,6 +186,7 @@ class CartView(View):
 #             {'message': 'ORDERED'},
 #             status=200
 #         )
+
 
 # class ShowOrdersView(View):
 #     @utils.signin_decorator
@@ -204,31 +215,37 @@ class CartView(View):
 class WishView(View):
     @utils.signin_decorator
     def post(self, request):
-        data    = json.loads(request.body)
-        user_id = request.user.id
+        try:
+            data    = json.loads(request.body)
+            user_id = request.user.id
 
-        if WishProduct.objects.filter(user = user_id, product = data['product_id']).exists():
-            WishProduct.objects.get(user = user_id, product = data['product_id']).delete()
+            if WishProduct.objects.filter(user = user_id, product = data['product_id']).exists():
+                WishProduct.objects.get(user = user_id, product = data['product_id']).delete()
 
+                return JsonResponse(
+                    {'message': 'REMOVED'},
+                    status = 200
+                )
+
+            else:
+                WishProduct(
+                    user_id    = user_id,
+                    product_id = data['product_id']
+                ).save()
+
+                return JsonResponse(
+                    {"message":"ADDED_TO_WISHLIST"},
+                    status = 201
+                )
+
+        except KeyError:
             return JsonResponse(
-                {'message': 'REMOVED'},
-                status = 200
-            )
-
-        else:
-            WishProduct(
-                user_id    = user_id,
-                product_id = data['product_id']
-            ).save()
-
-            return JsonResponse(
-                {'message':'ADDED_TO_WISHLIST'},
-                status = 201
+                {"message":"KEY_ERROR"},
+                status=400
             )
 
     @utils.signin_decorator
     def get(self, request):
-        
         user_id         = request.user.id
         wish_products   = [product for product in WishProduct.objects.filter(user = user_id).values()]
         wish_product    = [products['product_id'] for products in wish_products]            
@@ -270,19 +287,26 @@ class WishView(View):
 class RecentlyViewedView(View):
     @utils.signin_decorator
     def post(self, request):
-        data            = json.loads(request.body)
-        user_id         = request.user.id
-        view_user       = User.objects.get(id = user_id)
-        viewed_product  = Product.objects.get(id = data['product_id'])
+        try:
+            data            = json.loads(request.body)
+            user_id         = request.user.id
+            view_user       = User.objects.get(id = user_id)
+            viewed_product  = Product.objects.get(id = data['product_id'])
 
-        ViewedProduct(
-            user    = view_user,
-            product = viewed_product
-        ).save()
+            ViewedProduct(
+                user    = view_user,
+                product = viewed_product
+            ).save()
 
-        return JsonResponse(
-            {'MESSAGE':'ADDED_TO_VIEWED_LIST'},
-            status = 200)
+            return JsonResponse(
+                {"message":"ADDED_TO_VIEWED_LIST"},
+                status = 200)
+        
+        except KeyError:
+            return JsonResponse(
+                {"message":"KEY_ERROR"},
+                status=400
+            )
 
     @utils.signin_decorator
     def get(self, request):
@@ -298,6 +322,6 @@ class RecentlyViewedView(View):
             showing_list = product_list
 
         return JsonResponse(
-            {'VIEWED LIST': showing_list},
+            {"viewed_list": showing_list},
             status = 200
         )
